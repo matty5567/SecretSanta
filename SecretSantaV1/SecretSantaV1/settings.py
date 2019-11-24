1ec87e3434f75f8c2c65ecdea9310a8d6a4ac345
matty5567 <56730929+matty5567@users.noreply.github.com>
2019-11-24 14:48:52 +0000

    Update settings.py

--- SecretSantaV1/SecretSantaV1/settings.py
+++ SecretSantaV1/SecretSantaV1/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'i%06y2q&4l-!nv*8oolv470b!o)!xg*^9f7^d=q10#b$wd%c_e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['matt.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
